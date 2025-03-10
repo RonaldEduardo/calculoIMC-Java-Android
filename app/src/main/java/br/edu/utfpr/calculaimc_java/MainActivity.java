2a14379210117d510f23824d6ce42c39309cd6e3
RonaldGustmann <ronald29eduardo@gmail.com>
2025-03-10 11:36:28 -0300

    ajuste dos calculas de IMC

--- app/src/main/java/br/edu/utfpr/calculaimc_java/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/calculaimc_java/MainActivity.java
@@ -1,6 +1,10 @@
 package br.edu.utfpr.calculaimc_java;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,52 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etPeso;
+    private EditText etAltura;
+    private TextView tvResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        etPeso = findViewById(R.id.etPeso);
+        etAltura = findViewById(R.id.etAltura);
+        tvResultado = findViewById(R.id.tvResultado);
+        Button btnCalcular = findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcularIMC();
+            }
         });
+    }
+
+    public void calcularIMC(){
+        String pesoStr = etPeso.getText().toString();
+        String alturaStr = etAltura.getText().toString();
+
+        if (!pesoStr.isEmpty() && !alturaStr.isEmpty()) {
+            double peso = Double.parseDouble(pesoStr);
+            double altura = Double.parseDouble(alturaStr);
+            double imc = peso / (altura * altura);
+
+            String classificacao;
+            if (imc < 18.5) {
+                classificacao = "Abaixo do peso";
+            } else if (imc < 24.9) {
+                classificacao = "Peso normal";
+            } else if (imc < 29.9) {
+                classificacao = "Sobrepeso";
+            } else {
+                classificacao = "Obesidade";
+            }
+
+            String resultado = String.format("%.2f\n%s", imc, classificacao);
+            tvResultado.setText(resultado);
+        } else {
+            tvResultado.setText("Por favor, preencha todos os campos.");
+        }
     }
 }
